3e566f02a9ede017e66087dc7a7f39a37a99aa36
tamerWassim <tamer.wassim4141@gmail.com>
2020-08-11 18:52:09 +0100

    sign up changes

--- app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView data;
+    private Button help;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        help= findViewById(R.id.emergency);
         data = (TextView) findViewById(R.id.data);
         data.setText(getIntent().getStringExtra("un").toString() + "\n" +
                 getIntent().getStringExtra("pw").toString());
     }
+
 }
--- app/src/main/java/com/example/emergencyapp/SignUp.java
+++ app/src/main/java/com/example/emergencyapp/SignUp.java
@@ -3,12 +3,74 @@ package com.example.emergencyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SignUp extends AppCompatActivity {
 
+    NumberPicker day;
+    NumberPicker month;
+    NumberPicker year;
+
+    Button sign;
+
+    String val;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        sign = (Button) findViewById(R.id.sign);
+
+        day = (NumberPicker) findViewById(R.id.day);
+        day.setValue(1);
+        day.setMinValue(1);
+        day.setMaxValue(31);
+        day.setWrapSelectorWheel(false);
+        day.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                val = newVal + "/";
+            }
+        });
+
+        month = (NumberPicker) findViewById(R.id.month);
+        month.setValue(1);
+        month.setMinValue(1);
+        month.setMaxValue(12);
+        month.setWrapSelectorWheel(false);
+        month.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                val = newVal + "/";
+            }
+        });
+
+        year = (NumberPicker) findViewById(R.id.year);
+        year.setValue(2000);
+        year.setMinValue(1940);
+        year.setMaxValue(Calendar.getInstance().get(Calendar.YEAR));
+        year.setWrapSelectorWheel(false);
+        year.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                val = newVal + "";
+            }
+        });
+        //val = day.getValue() + "";
+
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast t = Toast.makeText(getBaseContext(), val, Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
+
     }
 }
